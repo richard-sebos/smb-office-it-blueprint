3c878eb14e93526cf4b7207f743398dd1a2d9ee9
Richard Chamberlain <info@sebostechnology.com>
2025-12-24 15:48:16 -0600

    Trying to get the right version to so

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -4,23 +4,30 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  base: '/smb-office-it-blueprint/', // 👈 Required for GitHub Pages
+
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight'
+        }
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Example Guide', slug: 'guides/example' }
+          ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' }
+        }
+      ]
+    })
+  ]
 });
